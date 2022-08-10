41235df6f6b25c27dd25d70b9cf4c72dc4a45b99
user <user@this.com>
2022-08-10 15:17:46 -0400

    Added dataservice

---
+++ spencermccdataservice/src/main/java/com/bah/msd/mcc/SpencermccdataserviceApplication.java
@@ -0,0 +1,13 @@
+package com.bah.msd.mcc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpencermccdataserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpencermccdataserviceApplication.class, args);
+	}
+
+}
